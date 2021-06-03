a21e58a69d132b11fb7b214c97ec462a462245af
Adam Perry <adam@itg.co.uk>
2021-06-03 09:01:13 +0100

    Move to Lambda runtime node12.x as node10.x is no longer supported

--- main.tf
+++ main.tf
@@ -174,7 +174,7 @@ resource "aws_lambda_function" "dashboard_lambda" {
   role             = aws_iam_role.dashboard_role.arn
   handler          = "index.handler"
   source_code_hash = data.archive_file.source.output_base64sha256
-  runtime          = "nodejs10.x"
+  runtime          = "nodejs12.x"
   description      = "MANAGED BY TERRAFORM"
 
   memory_size = "512"
